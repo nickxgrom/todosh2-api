ff721fedb0a2b2dcd778dd1b2d1bb5538f028110
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-11 01:16:47 +0600

    update(token): sign jwt-token

--- src/modules/user/service.js
+++ src/modules/user/service.js
@@ -1,5 +1,6 @@
 const User = require('./model')
 const ServiceError = require("../../../util/ServiceError");
+const jwt = require("jsonwebtoken");
 
 async function isUserExist(username) {
     if (await User.findOne({ where: { username }})) {
@@ -7,15 +8,26 @@ async function isUserExist(username) {
     }
 }
 
+async function logIn(id, username) {
+    // default algorithm hs256
+    const token = await jwt.sign({
+            id,
+            username,
+        },
+        // TODO: change secret on the production and move to .env
+        "todooshJwtSecret",
+        { expiresIn: 60*60*24 }
+    )
+    return token
+}
+
 module.exports = {
     createUser: async (username, password) => {
         await isUserExist(username)
         // TODO: add hash-function to hash a password
-        await User.create({ username, password })
-        // TODO: add function to sign and generate a token and refresh-token
+        const user = await User.create({ username, password })
         return {
-            'token': `some-token-${new Date().getTime()}`,
-            'refresh-token': `some-refresh-token-${new Date().getTime()}`
+            token: await logIn(user.id, user.username),
         }
     },
 
